8b9b94cfe889f3197cafed367bee067fcd4a9a04
Arty <Artylifeart@gmail.com>
2024-12-02 12:53:49 +0200

    remove ./srcs

--- src/stores/footerStore.js
+++ src/stores/footerStore.js
@@ -17,7 +17,7 @@ export const useFooterItemsStore = defineStore('footerItems', () => {
 
         {
             id: 2,
-            icon: '/assets/icons/footer/twitter-icon.svg',
+            icon: 'assets/icons/footer/twitter-icon.svg',
             icon_white: 'assets/icons/footer/twitter-white-icon.svg',
             link: '/',
             icon_description: 'Twitter'
@@ -25,7 +25,7 @@ export const useFooterItemsStore = defineStore('footerItems', () => {
 
         {
             id: 3,
-            icon: '/assets/icons/footer/instagram-icon.svg',
+            icon: 'assets/icons/footer/instagram-icon.svg',
             icon_white: 'assets/icons/footer/instagram-white-icon.svg',
             link: '/',
             icon_description: 'Instagram'
@@ -33,7 +33,7 @@ export const useFooterItemsStore = defineStore('footerItems', () => {
 
         {
             id: 4,
-            icon: '/assets/icons/footer/youtube-icon.svg',
+            icon: 'assets/icons/footer/youtube-icon.svg',
             icon_white: 'assets/icons/footer/youtube-white-icon.svg',
             link: '/',
             icon_description: 'Youtube'
@@ -41,7 +41,7 @@ export const useFooterItemsStore = defineStore('footerItems', () => {
 
         {
             id: 5,
-            icon: '/assets/icons/footer/google-sheets-icon.svg',
+            icon: 'assets/icons/footer/google-sheets-icon.svg',
             icon_white: 'assets/icons/footer/google-sheets-white-icon.svg',
             link: '/',
             icon_description: 'Google sheets'
@@ -49,7 +49,7 @@ export const useFooterItemsStore = defineStore('footerItems', () => {
 
         {
             id: 6,
-            icon: '/assets/icons/footer/share-icon.svg',
+            icon: 'assets/icons/footer/share-icon.svg',
             icon_white: 'assets/icons/footer/share-white-icon.svg',
             link: '/',
             icon_description: 'Share'
